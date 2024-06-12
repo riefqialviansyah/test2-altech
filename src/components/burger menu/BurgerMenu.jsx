7596ddf928f15cbb207faa022465cc9cd9bf16e6
riefqialviansyah <riefqi.alviansyah@gmail.com>
2024-06-12 19:38:25 +0800

    done mobile view

--- src/components/burger menu/BurgerMenu.jsx
+++ src/components/burger menu/BurgerMenu.jsx
@@ -4,7 +4,7 @@ export default function BurgerMenu() {
   return (
     <div className="burger-menu-component">
       <div className="burger-option">
-        <li>Register</li>
+        <li className="login-btn-burger">Login</li>
         <li>About</li>
         <li>Pricing</li>
         <li>Contact</li>
